dec8ab0cc59b85e549d69673db3d28af79caf155
0xm45kf4c3 <zakariaatais08@gmail.com>
2025-07-13 19:27:17 +0100

    .
    ,

--- permute.c
+++ permute.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 
+void	sort(char *str)
+{
+	char	tmp;
+
+	for (int i = 0; str[i]; i++)
+	{
+		for (int j = i + 1; str[j]; j++)
+		{
+			if (str[i] > str[j])
+			{
+				tmp = str[i];
+				str[i] = str[j];
+				str[j] = tmp;
+			}
+		}
+	}
+}
+
 void	swap(char *x, char *y)
 {
 	char	temp;
@@ -31,6 +49,7 @@ int	main(int ac, char **av)
 
 	if (ac == 1)
 		return (puts(av[1]), 0);
+	sort(av[1]);
 	n = strlen(av[1]);
 	permute(av[1], 0, n - 1);
 	return (0);
--- powerset.c
+++ powerset.c
@@ -1,53 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        return 0;
-    }
+int	parse_arguments(int argc, char *argv[], int *target, int **numbers)
+{
+	int	count;
 
-    int target = atoi(argv[1]);
-    int n = argc - 2;
-    int *numbers = NULL;
+	if (argc < 2)
+		return (-1);
+	*target = atoi(argv[1]);
+	count = argc - 2;
+	if (count > 0)
+	{
+		*numbers = (int *)malloc(count * sizeof(int));
+		if (!*numbers)
+			return (-1);
+		for (int i = 0; i < count; i++)
+		{
+			(*numbers)[i] = atoi(argv[i + 2]);
+		}
+	}
+	else
+		*numbers = NULL;
+	return (count);
+}
 
-    if (n > 0) {
-        numbers = (int *)malloc(n * sizeof(int));
-        if (numbers == NULL) {
-            return 1;
-        }
-        for (int i = 0; i < n; i++) {
-            numbers[i] = atoi(argv[i + 2]);
-        }
-    }
+int	calculate_subset_sum(int *numbers, int count, unsigned int n)
+{
+	int	sum;
 
-    unsigned int total_subsets = 1u << n;
-    for (unsigned int bitmask = 0; bitmask < total_subsets; bitmask++) {
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            if (bitmask & (1u << i)) {
-                sum += numbers[i];
-            }
-        }
+	sum = 0;
+	for (int i = 0; i < count; i++)
+	{
+		if (n & (1u << i))
+			sum += numbers[i];
+	}
+	return (sum);
+}
 
-        if (sum == target) {
-            int first = 1;
-            for (int i = 0; i < n; i++) {
-                if (bitmask & (1u << i)) {
-                    if (first) {
-                        printf("%d", numbers[i]);
-                        first = 0;
-                    } else {
-                        printf(" %d", numbers[i]);
-                    }
-                }
-            }
-            printf("\n");
-        }
-    }
+void	print_subset(int *numbers, int count, unsigned int n)
+{
+	int	first;
 
-    if (numbers) {
-        free(numbers);
-    }
+	first = 1;
+	for (int i = 0; i < count; i++)
+	{
+		if (n & (1u << i))
+		{
+			if (first)
+			{
+				printf("%d", numbers[i]);
+				first = 0;
+			}
+			else
+				printf(" %d", numbers[i]);
+		}
+	}
+	printf("\n");
+}
 
-    return 0;
+void	find_subset_sums(int *numbers, int count, int target)
+{
+	unsigned int	total_subsets;
+	int				sum;
+
+	total_subsets = 1u << count;
+	for (unsigned int n = 0; n < total_subsets; n++)
+	{
+		sum = calculate_subset_sum(numbers, count, n);
+		if (sum == target)
+			print_subset(numbers, count, n);
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int	target;
+	int	*numbers;
+	int	count;
+
+	numbers = NULL;
+	count = parse_arguments(argc, argv, &target, &numbers);
+	if (count < 0)
+		return (1);
+	find_subset_sums(numbers, count, target);
+	free(numbers);
+	return (0);
 }
